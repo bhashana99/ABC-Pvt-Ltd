2714c29b3bd75b7e4621e7af2ad9894245b4d204
Bhashana Chamodya <bhashanachamodya99@gmail.com>
2023-09-24 15:10:50 +0530

    connect to database

---
+++ config.php
@@ -0,0 +1,33 @@
+<?php
+
+class Database{
+
+     //$dsn = data source network
+     private $dsn = "mysql:host=localhost;port=3307;dbname=abc_pvt_ltd";
+     private $dbuser = "root";
+     private $dbpass = "";
+     
+     public $conn;
+ 
+     public function __construct(){
+         try{
+             $this->conn = new PDO($this->dsn,$this->dbuser,$this->dbpass);
+ 
+             echo 'Connected Successfully to the database!';
+ 
+         }catch (PDOException $e){
+             echo 'Error: '.$e->getMessage();
+         }
+ 
+         return $this->conn;
+     }
+ 
+}
+
+
+$checkDatabaseIsConnect = new Database();
+
+
+
+
+?>
